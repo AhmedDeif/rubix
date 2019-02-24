9a503739df92cd5ed3eba432763eda963ce833a2
Doola <ahmed.adel.abodeif@gmail.com>
2019-02-24 20:59:34 +0200

    show dynamic menu items

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+  Input, Menu
+} from 'semantic-ui-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class MainMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: ["item1","item2"]
+    };
+  }
+
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state
+    const children = this.state.menu
+    const Children = children.map((child) =>
+<Menu.Item name={child} active={activeItem === child} onClick={this.handleItemClick}/>)
+    return (
+        <div>
+          <Menu pointing>
+          {Children}
+            <Menu.Menu position='right'>
+              <Menu.Item>
+                <Input icon='search' placeholder='Search...' />
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+
+          <Segment>
+
+          </Segment>
+        </div>
+    );
+  }
+}
+
+ReactDOM.render(<MainMenu />, document.getElementById("root"));
